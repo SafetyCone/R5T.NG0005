04c520341357fc1ecb02965e830d0f9ec0b636df
davidcoats <david@getsafetycone.com>
2022-10-05 13:47:57 -0700

    Initial commit.

---
+++ source/R5T.NG0005/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.NG0005
+{
+	/// <summary>
+	/// Octokit NuGet package selector library.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.NG0005/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.NG0005
+Octokit NuGet package selector library.
